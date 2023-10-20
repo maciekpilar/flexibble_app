35989a29e258faf7e270c679cec5d41211be09a2
Maciej Pilarski <maciekpilar12@gmail.com>
2023-10-21 00:20:32 +0200

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,94 @@
+export const NavLinks = [
+  { href: "/", key: "Inspiration", text: "Inspiration" },
+  { href: "/", key: "Find Projects", text: "Find Projects" },
+  { href: "/", key: "Learn Development", text: "Learn Development" },
+  { href: "/", key: "Career Advancement", text: "Career Advancement" },
+  { href: "/", key: "Hire Developers", text: "Hire Developers" },
+];
+
+export const categoryFilters = [
+  "Frontend",
+  "Backend",
+  "Full-Stack",
+  "Mobile",
+  "UI/UX",
+  "Game Dev",
+  "DevOps",
+  "Data Science",
+  "Machine Learning",
+  "Cybersecurity",
+  "Blockchain",
+  "E-commerce",
+  "Chatbots",
+];
+
+export const footerLinks = [
+  {
+    title: "For developers",
+    links: [
+      { linkTitle: "Go Pro!", href: "/" },
+      { linkTitle: "Explore development work", href: "/" },
+      { linkTitle: "Development blog", href: "/" },
+      { linkTitle: "Code podcast", href: "/" },
+      { linkTitle: "Open-source projects", href: "/" },
+      { linkTitle: "Refer a Friend", href: "/" },
+      { linkTitle: "Code of conduct", href: "/" },
+    ],
+  },
+  {
+    title: "Hire developers",
+    links: [
+      { linkTitle: "Post a job opening", href: "/" },
+      { linkTitle: "Post a freelance project", href: "/" },
+      { linkTitle: "Search for developers", href: "/" },
+    ],
+  },
+  {
+    title: "Brands",
+    links: [{ linkTitle: "Advertise with us", href: "/" }],
+  },
+  {
+    title: "Company",
+    links: [
+      { linkTitle: "About", href: "/" },
+      { linkTitle: "Careers", href: "/" },
+      { linkTitle: "Support", href: "/" },
+      { linkTitle: "Media kit", href: "/" },
+      { linkTitle: "Testimonials", href: "/" },
+      { linkTitle: "API", href: "/" },
+      { linkTitle: "Terms of service", href: "/" },
+      { linkTitle: "Privacy policy", href: "/" },
+      { linkTitle: "Cookie policy", href: "/" },
+    ],
+  },
+  {
+    title: "Directories",
+    links: [
+      { linkTitle: "Development jobs", href: "/" },
+      { linkTitle: "Developers for hire", href: "/" },
+      { linkTitle: "Freelance developers for hire", href: "/" },
+      { linkTitle: "Tags", href: "/" },
+      { linkTitle: "Places", href: "/" },
+    ],
+  },
+  {
+    title: "Development assets",
+    links: [
+      { linkTitle: "Code Marketplace", href: "/" },
+      { linkTitle: "GitHub Marketplace", href: "/" },
+      { linkTitle: "NPM Registry", href: "/" },
+      { linkTitle: "Packagephobia", href: "/" },
+    ],
+  },
+  {
+    title: "Development Resources",
+    links: [
+      { linkTitle: "Freelancing", href: "/" },
+      { linkTitle: "Development Hiring", href: "/" },
+      { linkTitle: "Development Portfolio", href: "/" },
+      { linkTitle: "Development Education", href: "/" },
+      { linkTitle: "Creative Process", href: "/" },
+      { linkTitle: "Development Industry Trends", href: "/" },
+    ],
+  },
+];
